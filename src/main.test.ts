caf8f0428cfeca712ddc5c6fcc29256f9059f7ee
Cosmin Vasilache <cosminvasilache@yahoo.com>
2020-08-13 09:43:30 +0300

    Add jasmine for testing. Change README.md.

---
+++ src/main.test.ts
@@ -0,0 +1,12 @@
+describe('Test', () => {
+
+    beforeEach(() => {
+        console.log('before');
+    });
+
+    it('works', () => {
+        console.log('it');
+
+        expect(true).toBe(true);
+    });
+});
