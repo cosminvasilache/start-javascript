550addc989f22bae0fb6bc98770ff56fe37a45f8
Cosmin Vasilache <contact@cosminvasilache.dev>
2021-03-11 00:09:41 +0200

    Update webpack.config.ts to return a function.
    Receive environment variables.

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,49 +5,59 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
-const config: webpack.Configuration = merge({
-    mode: 'development',
-    devtool: 'source-map',
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".mjs", ".cjs", ".json", ".wasm"],
-    },
-    entry: {
-        main: './src/main.ts',
-    },
-    output: {
-        filename: '[name].[contenthash].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: ['html-loader'],
-            },
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            minify: {},
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-        }),
-        new CleanWebpackPlugin(),
-    ],
-});
+interface Environment {
+    dev: boolean,
+}
 
-export default config;
+function createConfig(env: Environment) {
+    console.log('ENVIRONMENT:', env);
+
+    const config: webpack.Configuration = merge({
+        mode: 'development',
+        devtool: 'source-map',
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".mjs", ".cjs", ".json", ".wasm"],
+        },
+        entry: {
+            main: './src/main.ts',
+        },
+        output: {
+            filename: '[name].[contenthash].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    use: ['html-loader'],
+                },
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                minify: {},
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css',
+            }),
+            new CleanWebpackPlugin(),
+        ],
+    });
+
+    return config;
+}
+
+export default createConfig;
